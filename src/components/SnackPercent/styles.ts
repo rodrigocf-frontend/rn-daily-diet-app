eb4698436e97e27e5cc99b266d7f0e776160a66b
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-06 15:50:49 -0300

    style: adjust text alignment

--- src/components/SnackPercent/styles.ts
+++ src/components/SnackPercent/styles.ts
@@ -1,19 +1,20 @@
+import { ArrowDownRightIcon } from "phosphor-react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  justify-content: center;
-  align-items: center;
-  padding: 0 0 40px 0;
+  margin: 0 auto;
 `;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.font.NUNITO_BOLD};
   font-size: ${({ theme }) => theme.size["2XL"]}px;
   color: ${({ theme }) => theme.color.GRAY_100};
+  text-align: center;
 `;
 
 export const Subtitle = styled.Text`
   font-family: ${({ theme }) => theme.font.NUNITO_REGULAR};
   font-size: ${({ theme }) => theme.size.SM}px;
   color: ${({ theme }) => theme.color.GRAY_200};
+  text-align: center;
 `;
