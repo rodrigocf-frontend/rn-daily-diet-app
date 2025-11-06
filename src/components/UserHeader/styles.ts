d919b9668d9958157df138eb3f2f3e204ce7b18c
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-06 15:52:16 -0300

    style: fix padding header

--- src/components/UserHeader/styles.ts
+++ src/components/UserHeader/styles.ts
@@ -1,10 +1,11 @@
+import { ArrowDownRightIcon } from "phosphor-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
   flex-direction: row;
   justify-content: space-between;
-  padding: 0 24px;
+  padding: 20px 24px;
 `;
 
 export const Brand = styled.Image.attrs({
